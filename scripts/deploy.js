7e1d50ee9b0b53ac329883d4925f2aab5708d604
SohamKothari8398 <126755623+SohamKothari8398@users.noreply.github.com>
2023-08-09 21:16:03 +0530

    Full Upload of project files
    All files uploaded

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const { ethers } = require("hardhat");
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  const balance = await deployer.getBalance();
+  const Marketplace = await hre.ethers.getContractFactory("NFTMarketplace");
+  const marketplace = await Marketplace.deploy();
+  await marketplace.deployed();
+  
+  const data = {
+    address: marketplace.address,
+    abi: JSON.parse(marketplace.interface.format('json'))
+  }
+
+  console.log(balance);
+  console.log(`Marketplace deployed at ${marketplace.address}`);
+  // console.log(deployer);
+
+  //This writes the ABI and address to the mktplace.json
+  fs.writeFileSync('./src/Marketplace.json', JSON.stringify(data))
+  
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
